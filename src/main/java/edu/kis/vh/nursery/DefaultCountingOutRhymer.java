ae1f8dc18873be55c5a7e015ed14f3ba78794ef3
215825 <215825@CARBON.CTI.P.LODZ.PL>
2019-11-26 14:22:13 +0100

    Stale i annotacje

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,13 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-	private int[] numbers = new int[12];
+	public static final int SIZE = 12;
+	public static final int INT = -1;
+	public static final int MAX = 11;
 
-	public int total = -1;
+	private final int[] numbers = new int[SIZE];
+
+	public int total = INT;
 
 	public void countIn(int in) {
 		if (!isFull())
@@ -12,22 +16,22 @@ public class DefaultCountingOutRhymer {
 	}
 
 	public boolean callCheck() {
-		return total == -1;
+		return total == INT;
 	}
 		
 	public boolean isFull() {
-		return total == 11;
+		return total == MAX;
 	}
 		
 	protected int peekaboo() {
 		if (callCheck())
-			return -1;
+			return INT;
 		return numbers[total];
 	}
 			
 	public int countOut() {
 		if (callCheck())
-			return -1;
+			return INT;
 		return numbers[total--];
 	}
 
