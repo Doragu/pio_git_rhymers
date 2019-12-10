4d8425cc8a7e53884d66d311666fe8f51c9d5076
215693 <215693@CARBON.CTI.P.LODZ.PL>
2019-12-10 08:22:28 +0100

    Poprawki dla Akcesory i hermetyzacja

--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -13,6 +13,16 @@ public class Node {
 		return value;
 	}
 
+	public Node getNext() {
+		return next;
+	}
+
+	public void setNext(Node next) {
+		this.next = next;
+	}
+
+
+
 	public Node getPrev() {
 		return prev;
 	}
