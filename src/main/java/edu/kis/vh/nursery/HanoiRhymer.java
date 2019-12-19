33c847f7877e4632a5634af681a9a423a44b04c0
Krzysztof Chybicki <krysiek97@interia.eu>
2019-12-19 20:15:48 +0100

    Zadanie 1.5.6

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,13 +1,27 @@
 package edu.kis.vh.nursery;
 
+/**
+ *	Class HanoiRhymer
+ */
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
+	/**
+	 *	Amount of rejected numbers
+	 */
 	private int totalRejected = 0;//zle
 
+	/**
+	 *	Returns total amount of rejected items.
+	 * @return Amount of rejected numbers.
+	 */
 	public int reportRejected() {
 		return totalRejected;
 	}
 
+	/**
+	 *	Adds a number to a array.
+	 * @param in Number to be added.
+	 */
 	@Override
 	public void countIn(int in) {
 		if (!callCheck() && (in > peekaboo())) //zle
